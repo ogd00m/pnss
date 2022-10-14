72e0afcb384e5d38290fba5240311fc7c8d591b0
ogd00m <crazedstriker1998@gmail.com>
2022-10-15 06:00:32 +0700

    number five

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,5 @@ Route::add('GET', '/hello', [Controller\Site::class, 'hello'])
     ->middleware('auth');
 Route::add(['GET', 'POST'], '/signup', [Controller\Site::class, 'signup']);
 Route::add(['GET', 'POST'], '/login', [Controller\Site::class, 'login']);
+Route::add(['GET', 'POST'], '/', [Controller\Site::class, 'login']);
 Route::add('GET', '/logout', [Controller\Site::class, 'logout']);
